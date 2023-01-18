bd6aaa352946fda30cfb89222298ce88898d3378
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-18 12:43:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import os
 # Autenticación de OpenAI (oculta la clave en una variable de entorno)
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
+
 def detect_gpt3(prompt):
     generated_text = openai.Completion.create(
         engine="text-davinci-002",
